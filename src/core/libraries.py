36561b380fcaa5c944e6dfb0f4092211a2786e71
William Parker <wlhh.2001@gmail.com>
2022-12-03 22:27:00 -0500

    Introduciendo librerías

---
+++ src/core/libraries.py
@@ -0,0 +1,8 @@
+# Se importan las librerías que serán necesarias
+# Librerías para la gui 
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+from PySide6.QtWidgets import *
+from PySide6.QtSvgWidgets import *
+
+# Librerías para el sistema
