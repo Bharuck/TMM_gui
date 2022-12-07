e39a6883c6d3e73fb8029a5d0534f35bb3576ad0
William Parker <wlhh.2001@gmail.com>
2022-12-07 12:55:30 -0500

    Defininendo la lectura de los archivos JSON para las configuraciones

--- src/core/json_settings.py
+++ src/core/json_settings.py
@@ -8,4 +8,20 @@ class Settings(object):
     settings_path = os.path.normpath(os.path.join(app_path, json_file))
     if not os.path.isfile(settings_path):
         print(f"WARNING: \"settings.json\" not found! check in the folder {settings_path}")
-        
+
+    # Iniciando las configuraciones
+    def __init__(self):
+        super(Settings, self).__init__()
+        self.items = {}
+        self.deserialize()
+
+    # Escritura del archivo JSON
+    def serialize(self):
+        with open(self.settings_path, "w", encoding='utf-8') as write:
+            json.dump(self.items, write, indent=4)
+    
+    # Escritura del archivo JSON
+    def deserialize(self):
+        with open(self.settings_path, "r", encoding='utf-8') as reader:
+            settings = json.loads(reader.read())
+            self.items = settings
